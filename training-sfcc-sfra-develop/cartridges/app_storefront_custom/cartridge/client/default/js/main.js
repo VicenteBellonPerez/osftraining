4780a1422a15aaf4b7c15d2fbbee6bec30d92109
Vicente Bellón Pérez <vicente.bellon@3mtglobal.com>
2021-04-16 14:05:04 +0200

    bck_exercise3.2

--- training-sfcc-sfra-develop/cartridges/app_storefront_custom/cartridge/client/default/js/main.js
+++ training-sfcc-sfra-develop/cartridges/app_storefront_custom/cartridge/client/default/js/main.js
@@ -3,6 +3,5 @@ var processInclude = require('./util');
 
 $(document).ready(function () {
     processInclude(require('../../../../../app_storefront_base/cartridge/client/default/js/main'));
-    //processInclude(require('./components/test'));
-    //processInclude(require('./cart/cart'));
+    processInclude(require('./components/test'));
 });
